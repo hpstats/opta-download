9c45b65483290a507fb55249b867669219cceffb
hpstats <highperformancestats@gmail.com>
2017-02-21 13:17:57 +0000

    Working through matches

--- Matches.R
+++ Matches.R
@@ -16,7 +16,35 @@ for(i in 1:nrow(competition.table)){
   if (tries == 5){
     error <- address2
   } else {
+    fixtures.line <- comp.html[grep('See monthly fixtures', comp.html)]
+    fixtures.url <- substr(fixtures.line, 
+                    str_locate(fixtures.line, 'href=') + 6,
+                    str_locate(fixtures.line, 'class=') - 3)
     
+    address3 <- paste(address, fixtures.url, sep = '')
+    
+    tries <- 0
+    
+    while (tries < 5){
+      fixtures.html <- try(readLines(address3))
+      if(class(fixtures.html) == "try-error" | length(fixtures.html) <= 50){
+        tries <- tries + 1
+        Sys.sleep(5.0)
+      } else {
+        tries <- 10
+      }
+    }
+    
+    if (tries == 5){
+      error <- address
+    } else {
+      fixtures.block <- fixtures.html[grep('DataStore', fixtures.html):
+                                        (grep('stageFixtures.load', fixtures.html)[2]
+                                      - 2)]
+      
+      
+      
+    }
     
     
   }
